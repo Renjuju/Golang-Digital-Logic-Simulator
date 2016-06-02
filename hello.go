5bfc9fdeb078de00c4523b2cb6558e16b1d3a7d8
Renju Radhakrishnan <renju467@gmail.com>
2016-06-02 13:30:10 -0700

    file parser, test input file

--- hello.go
+++ hello.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "bufio"
+        "os"
+      )
 
 func main() {
-fmt.Println("Hello, ")
+  circuit, err:= readLines("input.txt")
+  check(err)
+  for i, circuit:= range circuit {
+    fmt.Println(i, circuit)
+  }
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+//http://stackoverflow.com/questions/5884154/golang-read-text-file-into-string-array-and-write
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  check(err)
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
 }
