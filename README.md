d4e39a0126861c0f0731bfde7afa84f1ea1df76b
Renju Radhakrishnan <renju467@gmail.com>
2016-06-05 15:35:32 -0400

    more channels + parsing

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Golang-Digital-Logic-Simulator
 For your final programming project you will implement a digital circuit simulator along the lines of what you designed for Exercise 8-4.  Your simulator should include support for the basic logic gates: NOT, AND, OR, NAND, NOR, and XOR.  You should also have a clock that generates a stream of pulses at some specified rate.  The final component you need to support is a D flip-flop.  The D flip-flop has two inputs and two outputs and operates as follows: when the clock input transitions from 0 to 1, the value on the D input is stored.  The Q output is the stored value of D and the Q with bar on top is the inverse of the stored D value.
 
-Your implementation should be written in go.  Each component (gate, clock, or flip-flop) will have an associated go routine, and the go routines will communicate using channels.  Your program should as the user for the following:
+Your implementation should be written in go.  Each component (gate, clock, or flip-flop) will have an associated go routine, and the go routines will communicate using channels.  Your program should ask the user for the following:
 
 Name of a file containing the description of the circuit to simulate
 Initial state values for flip-flops and for any external input signals
--- main.go
+++ main.go
@@ -3,31 +3,87 @@ package main
 import ("fmt"
         "bufio"
         "os"
+        "time"
         "sync"
+        "strings"
       )
 
-func printer(c1 chan string, c2 chan string) {
+var wg sync.WaitGroup
+
+func readGates(c4 chan []string) {
+  circuits := <-c4
+  println(circuits[0])
+  statements := strings.Split(circuits[0], "")
+  elements := []int{}
+  var tempVal int
+
+  // asks for intial values of inputs
+  for i, element:= range statements {
+    println(i, element)
+    println("What would you like the value of ", element, " to be ")
+    fmt.Scanln(&tempVal)
+    elements = append(elements, tempVal)
+  }
+
+  // uses gates to parse inputs
+  line := strings.Split(circuits[1], "+")
+  fmt.Println(line)
+  for i, element:=range line {
+    println(i, element)
+  }
+
+  wg.Done()
+}
+
+func clock(c3 chan string) {
+  var x int
+  x = 0
+  println("How much clock pulses per second?")
+  var pulsesPerSecond int
+  // fmt.Scanln(&pulsesPerSecond)
+  pulsesPerSecond = 3
+  c3<-"done"
+  for {
+    x++
+    // println(x)
+    time.Sleep(time.Second / time.Duration(pulsesPerSecond))
+    x--
+  }
+  wg.Done()
+}
+
+// prints and sends array of string to readGates
+func printer(c1 chan string, c2 chan string, c4 chan []string) {
   fileName:= <-c1
   circuit, err:= readLines(fileName)
   check(err)
-  for i, circuit:= range circuit {
-    fmt.Println(i, circuit)
-  }
+  // for i, circuit:= range circuit {
+  //   fmt.Println(i, circuit)
+  // }
+  c4<-circuit
+  wg.Done()
 }
 
-func reader(c1 chan string) {
+func reader(c1 chan string, c3 chan string) {
+  <-c3
   fmt.Println("What's the name of your file?")
   var fileName string
-  fmt.Scanln(&fileName)
+  // fmt.Scanln(&fileName)
+  fileName = "input.txt"
   go func() {
     c1 <- fileName
   }()
+  wg.Done()
 }
 
 func main() {
-  c1, c2, done := make(chan string), make(chan string), make(chan bool)
-  go reader(c1)
-  go printer(c1, c2)
+  c1, c2, c3, c4 := make(chan string), make(chan string), make(chan string), make(chan []string)
+  wg.Add(4)
+  go clock(c3)
+  go reader(c1, c3)
+  go printer(c1, c2, c4)
+  go readGates(c4)
+  wg.Wait()
 }
 
 //logic gates
@@ -47,13 +103,30 @@ func or(x, y bool) bool {
   }
 }
 
+func nand(x,y bool) bool {
+    if (!and(x, y)) {
+      return true
+    }
+    return false
+}
+
+func nor(x,y bool) bool {
+  return true
+}
+
+func xor() bool {
+  //return or(and(x, not(y), and(y, not(x))))
+  return true
+}
+
 func check(e error) {
     if e != nil {
-        panic(e)
+        // panic(e)
     }
 }
 
 // reads line by line
+// boiler plate code from
 //http://stackoverflow.com/questions/5884154/golang-read-text-file-into-string-array-and-write
 func readLines(path string) ([]string, error) {
   file, err := os.Open(path)
