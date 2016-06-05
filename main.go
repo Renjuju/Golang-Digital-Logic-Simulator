b4d1d75a1ef40f9e2c4d41df2bd53aecd4f2d4c5
Renju Radhakrishnan <renju467@gmail.com>
2016-06-04 20:30:48 -0700

    made read and print channels

--- main.go
+++ main.go
@@ -3,16 +3,32 @@ package main
 import ("fmt"
         "bufio"
         "os"
+        "sync"
       )
+var wg sync.WaitGroup
 
-func main() {
-  x := true
-  y := true
-  circuit, err:= readLines("input.txt")
+func printer(c1 chan string, c2 chan string) {
+  fileName:= <-c1
+  circuit, err:= readLines(fileName)
   check(err)
   for i, circuit:= range circuit {
     fmt.Println(i, circuit)
   }
+}
+
+func reader(c1 chan string) {
+  fmt.Println("What's the name of your file?")
+  var fileName string
+  fmt.Scanln(&fileName)
+  go func() {
+    c1 <- fileName
+  }()
+}
+
+func main() {
+  c1, c2, done := make(chan string), make(chan string), make(chan bool)
+  go reader(c1)
+  go printer(c1, c2)
 }
 
 //logic gates
@@ -38,6 +54,7 @@ func check(e error) {
     }
 }
 
+// reads line by line
 //http://stackoverflow.com/questions/5884154/golang-read-text-file-into-string-array-and-write
 func readLines(path string) ([]string, error) {
   file, err := os.Open(path)
